0290f3aa9627121c37580b3b2a3c8cee542dae24
justin leggett <justin@codingrookie.com>
2017-07-18 15:14:50 -0500

    added components folder and sections.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./base.css"; // eslint-disable-line import/no-unassigned-import
 import "semantic-ui-css/semantic.min.css";
+import Intro from "./sections/Intro/Intro";
+import Projects from "./sections/Projects/Projects";
+import Contact from "./sections/Contact/Contact";
+import Menu from "./components/menu";
+
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Menu />
+      <Intro />
+      <Projects />
+      <Contact />
     </div>
   );
 };
