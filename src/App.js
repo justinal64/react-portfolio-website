0e1239eca0a3654fd4485d963a1199aaf13ab29f
justin leggett <justin@codingrookie.com>
2017-07-18 14:44:37 -0500

    added image loader to Webpack.config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./base.css"; // eslint-disable-line import/no-unassigned-import
-
+import "semantic-ui-css/semantic.min.css";
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Hello World</h1>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  target: 'web',
-  devtool: 'cheap-eval-source-map',
+  target: "web",
+  devtool: "cheap-eval-source-map",
   entry: {
-    main: ['babel-polyfill', 'react-hot-loader/patch', './src/index.js'],
+    main: ["babel-polyfill", "react-hot-loader/patch", "./src/index.js"]
   },
   output: {
-    filename: '[name].[hash].js',
-    path: path.resolve('./dist'),
-    publicPath: '/',
+    filename: "[name].[hash].js",
+    path: path.resolve("./dist"),
+    publicPath: "/"
   },
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   enforce: 'pre',
-      //   exclude: ['node_modules'],
-      //   loader: 'eslint-loader',
-      // },
       {
         test: /\.js$/,
-        exclude: ['node_modules'],
-        loader: 'babel-loader',
+        exclude: ["node_modules"],
+        loader: "babel-loader"
       },
       {
         test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       },
-    ],
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
   },
   plugins: [
     // build optimization plugins
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'common',
-      filename: 'common.[hash].js',
+      name: "common",
+      filename: "common.[hash].js"
     }),
     new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-  ],
+      template: "index.html"
+    })
+  ]
 };
