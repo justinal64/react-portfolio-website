c3620bc66d782a8761c8e0f030ed8f5afa83846a
justin leggett <justin@codingrookie.com>
2017-07-20 12:03:21 -0500

    removed App.js page and moved routing logic to index.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,31 +1,27 @@
 import React, { Component } from "react";
 import { Menu, Segment } from "semantic-ui-react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Contact from "../sections/Contact/Contact";
+import Projects from "../sections/Projects/Projects";
+import Intro from "../sections/Intro/Intro";
+import NotFound from "../sections/NotFound/NotFound";
 
 export default class Navigation extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Intro</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-          </ul>
-
-          <hr />
-
-          <Route exact path="/" component={Intro} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/projects" component={Projects} />
-        </div>
-      </Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Intro</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navigation from "./components/navigation";
+import Contact from "./sections/Contact/Contact";
+import Projects from "./sections/Projects/Projects";
+import Intro from "./sections/Intro/Intro";
+import NotFound from "./sections/NotFound/NotFound";
 // import "./css/style.css";
 
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Router>
+    <div>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Intro} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/projects" component={Projects} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </div>
+  </Router>,
   document.getElementById("app")
 );
