d6294fccd1abfb1d89ea07b0e238e18238a8f018
justin leggett <justin@codingrookie.com>
2017-07-21 08:45:28 -0500

    cleaned up navigation

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,31 +1,24 @@
 import React, { Component } from "react";
-import { Menu, Segment } from "semantic-ui-react";
+import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import Contact from "../../sections/Contact/Contact";
-import Projects from "../../sections/Projects/Projects";
-import Intro from "../../sections/Intro/Intro";
-import NotFound from "../../sections/NotFound/NotFound";
 import "./navigation.css";
+
 export default class Navigation extends Component {
-  state = {};
-
-  handleItemClick = (e, { name }) => {
-    // go to route here.....
-    e.preventDefault();
-
-    this.setState({ activeItem: name });
-  };
   render() {
-    const { activeItem } = this.state;
     return (
-      <Menu fluid widths={7} size="massive" secondary>
-        <Menu.Item name="Intro" active={activeItem === "Intro"}>
-          <Link to="/">Intro</Link>
+      <Menu fluid size="massive" secondary>
+        <Menu.Menu position="left">
+          <Menu.Item name="Home">
+            <Link to="/">JL Designs</Link>
+          </Menu.Item>
+        </Menu.Menu>
+        <Menu.Item name="Home">
+          <Link to="/">Home</Link>
         </Menu.Item>
-        <Menu.Item name="Contact" active={activeItem === "Contact"}>
+        <Menu.Item name="Contact">
           <Link to="/contact">Contact</Link>
         </Menu.Item>
-        <Menu.Item name="Projects" active={activeItem === "Projects"}>
+        <Menu.Item name="Projects">
           <Link to="/projects">Projects</Link>
         </Menu.Item>
       </Menu>
