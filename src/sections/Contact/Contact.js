75ccd72ad960477c502e6b24b616f82b18575e96
justin leggett <justin@codingrookie.com>
2017-07-21 09:14:29 -0500

    changed background to an image and added onHover effect to navigation

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { Button, Icon } from "semantic-ui-react";
 import "./Contact.css";
 
 const Contact = () => {
   return (
     <div className="contact">
       <h1>Contact page</h1>
+      <div>
+        <Button color="facebook">
+          <Icon name="facebook" /> Facebook
+        </Button>
+        <Button color="twitter">
+          <Icon name="twitter" /> Twitter
+        </Button>
+        <Button color="google plus">
+          <Icon name="google plus" /> Google Plus
+        </Button>
+        <Button color="vk">
+          <Icon name="vk" /> VK
+        </Button>
+        <Button color="linkedin">
+          <Icon name="linkedin" /> LinkedIn
+        </Button>
+        <Button color="instagram">
+          <Icon name="instagram" /> Instagram
+        </Button>
+        <Button color="youtube">
+          <Icon name="youtube" /> YouTube
+        </Button>
+      </div>
+      <div>
+        <Button circular color="facebook" icon="facebook" />
+        <Button circular color="twitter" icon="twitter" />
+        <Button circular color="linkedin" icon="linkedin" />
+        <Button circular color="google plus" icon="google plus" />
+      </div>
     </div>
   );
 };
--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Grid, Image } from "semantic-ui-react";
 import "./Projects.css";
 
 const Projects = () => {
   return (
     <div className="projects">
       <h1>Projects page</h1>
+      <Grid>
+        <Grid.Row columns={4}>
+          <Grid.Column>
+            <Image src="/assets/images/wireframe/image.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="/assets/images/wireframe/image.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="/assets/images/wireframe/image.png" />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 };
