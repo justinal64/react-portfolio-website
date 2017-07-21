c7ffb34fbf66e89e4391ba8bad331f13e1f8c5cd
justin leggett <justin@codingrookie.com>
2017-07-21 11:33:06 -0500

    added intro to homepage

--- src/sections/Home/Home.js
+++ src/sections/Home/Home.js
@@ -7,7 +7,12 @@ const Home = () => {
     <div className="home">
       <Typist>
         <span className="h1">
-          Welcome To My <br />Portfolio Website
+          Hi my name is Justin Leggett and I am a full stack software developer
+          from Nashville Tennessee. I recently graduated from Nashville Software
+          School and now I am looking for my first full time gig in software
+          development. When I'm not creating super cool websites like this, you
+          will find me hanging out with my wife and playing with our children.
+          Thank you so much for visiting and have a great day.
         </span>
       </Typist>
     </div>
