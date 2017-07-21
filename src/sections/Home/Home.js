d551d1c8054fb3aef9977989077293585d07b674
justin leggett <justin@codingrookie.com>
2017-07-21 10:22:44 -0500

    added typist to homepage header

--- src/sections/Home/Home.js
+++ src/sections/Home/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Header } from "semantic-ui-react";
-
+import Typist from "react-typist";
+import "./Home.css";
 const Home = () => {
   return (
     <div className="home">
-      <Header as="h1" textAlign="center">
-        JL Designs
+      <Typist>
+        <Header as="h1" textAlign="center">
+          Welcome To My Portfolio Website
+        </Header>
+      </Typist>
+      {/* <Header as="h2" textAlign="center" className="headerR">
+        To My
       </Header>
+      <Header as="h1" textAlign="center" className="headerR">
+        Portfolio Website
+      </Header> */}
     </div>
   );
 };
