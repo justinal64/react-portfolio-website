bfdc41719e1001a061e9902eac0dbf76fc3125ad
justin leggett <justin@codingrookie.com>
2017-08-08 15:48:45 -0500

    moved contact info to contact.js

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -1,41 +1,36 @@
 import React from "react";
-import { Button, Icon } from "semantic-ui-react";
+import { Button, Icon, Segment, Header, Grid } from "semantic-ui-react";
 import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <h1>Contact page</h1>
-      <div>
-        <Button color="facebook">
-          <Icon name="facebook" /> Facebook
-        </Button>
-        <Button color="twitter">
-          <Icon name="twitter" /> Twitter
-        </Button>
-        <Button color="google plus">
-          <Icon name="google plus" /> Google Plus
-        </Button>
-        <Button color="vk">
-          <Icon name="vk" /> VK
-        </Button>
-        <Button color="linkedin">
-          <Icon name="linkedin" /> LinkedIn
-        </Button>
-        <Button color="instagram">
-          <Icon name="instagram" /> Instagram
-        </Button>
-        <Button color="youtube">
-          <Icon name="youtube" /> YouTube
-        </Button>
-      </div>
-      <div>
-        <Button circular color="facebook" icon="facebook" />
-        <Button circular color="twitter" icon="twitter" />
-        <Button circular color="linkedin" icon="linkedin" />
-        <Button circular color="google plus" icon="google plus" />
-      </div>
-    </div>
+    <Segment style={{ padding: "8em 0em" }} vertical inverted>
+      <Grid container stackable verticalAlign="middle">
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Header inverted as="h3" style={{ fontSize: "2em" }}>
+              Like what you see? Let's talk
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              In 2012 I was working for a healthcare company as a tech support
+              specialist. I was in a room with developers, and day after day I
+              watched these developers creating something out of nothing, so I
+              started researching software development and started creating
+              "Hello World" apps. After 6 months of tinkering my boss gave me a
+              very ambicious project "Can you create a dashboard that we will
+              display incoming data?" Over the next 6 months I dove deeper into
+              C#, asp.net, and javascript to create a dashboard application that
+              would display data in realtime. We purchased a TV and every
+              morning when I walked in the office I was proud to see what I
+              created hanging on the wall for everyone to see!
+            </p>
+          </Grid.Column>
+          <Grid.Column floated="right" width={6}>
+            {/* <Image bordered size="large" src={PHS} /> */}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   );
 };
 
--- src/sections/Home/Home.js
+++ src/sections/Home/Home.js
@@ -18,6 +18,8 @@ import PHS from "../../static/img/phs.png";
 import TBA from "../../static/img/tba.png";
 import PWS from "../../static/img/pws.png";
 import TSA from "../../static/img/tsa.png";
+import Contact from "../Contact/Contact";
+
 const FixedMenu = () =>
   <Menu fixed="top" size="large">
     <Container>
@@ -355,34 +357,7 @@ export default class HomepageLayout extends Component {
           </Container>
         </Segment>
         {/* Contact */}
-        <Segment style={{ padding: "8em 0em" }} vertical inverted>
-          <Grid container stackable verticalAlign="middle">
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Header inverted as="h3" style={{ fontSize: "2em" }}>
-                  Like what you see? Let's talk
-                </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  In 2012 I was working for a healthcare company as a tech
-                  support specialist. I was in a room with developers, and day
-                  after day I watched these developers creating something out of
-                  nothing, so I started researching software development and
-                  started creating "Hello World" apps. After 6 months of
-                  tinkering my boss gave me a very ambicious project "Can you
-                  create a dashboard that we will display incoming data?" Over
-                  the next 6 months I dove deeper into C#, asp.net, and
-                  javascript to create a dashboard application that would
-                  display data in realtime. We purchased a TV and every morning
-                  when I walked in the office I was proud to see what I created
-                  hanging on the wall for everyone to see!
-                </p>
-              </Grid.Column>
-              <Grid.Column floated="right" width={6}>
-                <Image bordered size="large" src={PHS} />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
+        <Contact />
 
         <Segment inverted vertical style={{ padding: "5em 0em" }}>
           <Container>
