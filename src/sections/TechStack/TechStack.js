5e8ff18e3ca9be48792b4f246bd4c85d9140af6d
justin leggett <justin@codingrookie.com>
2017-08-11 10:13:39 -0500

    added hover effect to all tech icons

--- src/sections/TechStack/TechStack.js
+++ src/sections/TechStack/TechStack.js
@@ -57,122 +57,188 @@ const TechStack = () => {
           <Grid>
             {/* Shows up on Mobile view only */}
             <Grid.Row columns={3} only="mobile">
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Angular} />
+                <p className="title">Angular</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Aspnet} />
+                <p className="title">.Net</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Bootstrap} />
+                <p className="title">Bootstrap</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={CSharp} />
+                <p className="title">C#</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Css3} />
+                <p className="title">CSS3</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Firebase} />
+                <p className="title">Firebase</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Git} />
+                <p className="title">Git</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={GruntLogo} />
+                <p className="title">Grunt</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Html5} />
+                <p className="title">Html5</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Javascript} />
+                <p className="title">Javascript</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Jquery} />
+                <p className="title">JQuery</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Npm} />
+                <p className="title">Npm</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Octocat} />
+                <p className="title">Github</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={ReactLogo} />
+                <p className="title">React</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Sass} />
+                <p className="title">Sass</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Sql} />
+                <p className="title">Sql</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={VisualStudio} />
+                <p className="title">Visual Studio</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Webpack} />
-              </Grid.Column>
-              <Grid.Column>
-                <img src={Xtocat} />
+                <p className="title">Webpack</p>
+                <div className="overlay" />
               </Grid.Column>
             </Grid.Row>
             {/* Computer/Tablet section used unless user is on a mobile device*/}
             <Grid.Row columns={6} only="computer tablet">
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Angular} />
+                <p className="title">Angular</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Aspnet} />
+                <p className="title">.Net</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Bootstrap} />
+                <p className="title">Bootstrap</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={CSharp} />
+                <p className="title">C#</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Css3} />
+                <p className="title">CSS3</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Firebase} />
+                <p className="title">Firebase</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Git} />
+                <p className="title">Git</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={GruntLogo} />
+                <p className="title">Grunt</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Html5} />
+                <p className="title">Html5</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Javascript} />
+                <p className="title">Javascript</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Jquery} />
+                <p className="title">JQuery</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Npm} />
+                <p className="title">Npm</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Octocat} />
+                <p className="title">Github</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={ReactLogo} />
+                <p className="title">React</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Sass} />
+                <p className="title">Sass</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Sql} />
+                <p className="title">Sql</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={VisualStudio} />
+                <p className="title">Visual Studio</p>
+                <div className="overlay" />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column className="overlayEffect">
                 <img src={Webpack} />
-              </Grid.Column>
-              <Grid.Column>
-                <img src={Xtocat} />
+                <p className="title">Webpack</p>
+                <div className="overlay" />
               </Grid.Column>
             </Grid.Row>
           </Grid>
